7dbe1c35b0307858f2236986a53f3e1a98106e97
twMisc <ccchang.am09g@nctu.edu.tw>
2023-03-21 17:03:49 +0800

    added interactive 誠 messages

--- bot.py
+++ bot.py
@@ -2,30 +2,41 @@ from pathlib import Path
 import random
 import discord
 from discord.ext import commands
+import re
 
 token = Path('token').read_text()
 MY_TOKEN = token
 
+dinner_candidtes = ['拉', '咖哩', '肯', '麥', '摩', '大的']
+
 intents = discord.Intents().all()
-bot = commands.Bot(command_prefix='$', intents=intents)
+client = commands.Bot(command_prefix='$', intents=intents)
 
 
-@bot.event
+@client.event
 async def on_ready():
     print(
-        f'\n\nSuccessfully logged into Discord as "{bot.user}"\nAwaiting user input...'
+        f'\n\nSuccessfully logged into Discord as "{client.user}"\nAwaiting user input...'
     )
-    await bot.change_presence(status=discord.Status.online,
-                              activity=discord.Activity(
-                                  type=discord.ActivityType.playing,
-                                  name="all by myself..."))
+    await client.change_presence(status=discord.Status.online,
+                                 activity=discord.Activity(
+                                     type=discord.ActivityType.playing,
+                                     name="all by myself..."))
 
 
-@bot.hybrid_command()
+@client.hybrid_command()
 async def dinner(ctx):
-    food_string = ['拉', '咖哩', '肯', '麥', '摩', '大的']
-    food = random.choice(food_string)
+    food = random.choice(dinner_candidtes)
     await ctx.send(food)
 
 
-bot.run(MY_TOKEN)
+@client.event
+async def on_message(message):
+    if message.content.startswith("誠"):
+        if "晚餐" in message.content:
+            await message.channel.send(random.choice(dinner_candidtes))
+        else:
+            await message.channel.send("= =")
+
+
+client.run(MY_TOKEN)
