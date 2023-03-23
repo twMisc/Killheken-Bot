c65b3cce4cf0301bf8c821c84188756ffd54ab37
twMisc <ccchang.am09g@nctu.edu.tw>
2023-03-23 23:24:04 +0800

    嘗試更新式回應率

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import json
 from pathlib import Path
 import random
 import re
+import time
+import math
 import discord
 import subprocess
 from discord.ext import commands
@@ -22,7 +24,7 @@ MY_GUILD_ID = discord.Object(guild)
 
 dinner_candidates = ['拉', '咖哩', '肯', '麥', '摩', '大的']
 Response_list = ['誠', '大', '豪', '翔', '抹茶']
-REPLY_RATE = 0.8
+REPLY_RATE = 0.65
 intents = discord.Intents().all()
 client = commands.Bot(command_prefix='$', intents=intents)
 client.owner_ids = ADMIN_LIST
@@ -30,12 +32,22 @@ client.owner_ids = ADMIN_LIST
 def emoji(emoji: dict):
     return f"<:{emoji['name']}:{emoji['id']}>"
 
+def t_func(t):
+    if t<5*60:
+        output = 0.7/(1+math.exp((t-60*5)/60)) + 0.3
+    else:
+        output = 0.7/(1+math.exp((t-60*5)/20)) + 0.3
+    return output
 
 @client.event
 async def on_ready():
     print(
         f'\n\nSuccessfully logged into Discord as "{client.user}"\nAwaiting user input...'
     )
+    global t_old, t_new
+    t_old = time.time()
+    t_new = t_old
+
     await client.change_presence(status=discord.Status.online,
                                  activity=discord.Activity(
                                      type=discord.ActivityType.playing,
@@ -65,6 +77,14 @@ async def update(ctx):
     await ctx.send('Updating bot....')
     _ = subprocess.call(["bash", "/home/ubuntu/update_bot.sh"])
 
+@client.hybrid_command(name='rate',
+                       description='輸出帥哥誠的回應率')
+async def rate(ctx):
+    t_new = time.time()
+    t_span = max(60*60, t_new-t_old)
+    REPLY_RATE = t_func(t_span)
+    await ctx.send('`帥哥誠現在的回應率是: {REPLY_RATE:.3f}`')
+
 
 @client.event
 async def on_command_error(ctx, exception):
@@ -76,7 +96,13 @@ async def on_command_error(ctx, exception):
 
 @client.event
 async def on_message(message):
+    global t_old, t_new, REPLY_RATE
     if message.content.startswith("誠"):
+        t_new = time.time()
+        t_span = max(60*60, t_new-t_old)
+        REPLY_RATE = t_func(t_span)
+        t_old = t_new
+
         if "晚餐" in message.content:
             await message.channel.send(random.choice(dinner_candidates))
         elif "還是" in message.content:
