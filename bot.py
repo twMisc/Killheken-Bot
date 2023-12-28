509ff2c3b1ac8378b58883beda82910c57b56e2e
twMisc <ccchang.am09g@nctu.edu.tw>
2023-12-28 17:02:34 +0800

    Set conversation_id for bard

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ session = requests.Session()
 session.cookies.set("__Secure-1PSID", cookie_dict['__Secure-1PSID'])
 session.cookies.set("__Secure-1PSIDTS", cookie_dict['__Secure-1PSIDTS'])
 session.headers = SESSION_HEADERS
-bard = BardCookies(cookie_dict=cookie_dict, session=session, conversation_id='帥哥誠的煩惱')
+bard = BardCookies(cookie_dict=cookie_dict, session=session, conversation_id='c_2e5b34f1bae27158')
 
 ADMIN_LIST = set(admins)
 MY_TOKEN = token
